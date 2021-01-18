986ee7485a27d38e6843b8ef130c68411b935e61
jacobmcf <jacobmcf@student.ubc.ca>
2021-01-18 11:13:12 -1000

    static implementation of first graph

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import dash
+from dash_html_components.Div import Div
 import pandas as pd
 import altair as alt
 import dash_core_components as dcc
 import dash_html_components as html
 
 # Importing Data
-pd.read_csv('data/kaggle_survey_2020_responses.csv')
+kaggle_survey = pd.read_csv('MDS/532/career_deciscion_aid/data/kaggle_survey_2020_responses.csv',
+ skiprows=[1])
+
+language_columns = kaggle_survey.filter(regex = 'Q7', axis =1).columns.to_list()
+kaggle_langs = kaggle_survey.melt(
+    id_vars = 'Q5', value_vars = language_columns, value_name = 'prog_lang').dropna()
+
+alt.data_transformers.disable_max_rows()
+
+lang_chart = alt.Chart(data = kaggle_langs).mark_bar().encode(
+    x = 'prog_lang',
+    y = 'count()')
 
 app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
@@ -17,8 +29,13 @@ app.layout = html.Div([
         {'label' : 'Data Scientist',
         'value' : 'Data Scientist'},
         {'label' : 'Software Engineeer',
-        'value' : 'Software Engineer'}
-    ])])])
+        'value' : 'Software Engineer'}])]),
+    # Language Chart
+    html.Div(html.Iframe(srcDoc = lang_chart.to_html(), style = {
+        'border-width' : '0',
+        'width' : '100%',
+        'height' : '400px'}))
+    ])
 
 
 if __name__ == '__main__':
